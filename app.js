6f0afd9ce7e1237dcda3cf6e9ca5f0a7c774cac8
Juliana <rojasnpaula@javeriana.edu.co>
2021-09-17 20:11:58 -0500

    skills section with cards

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const links = document.querySelectorAll(".nav-links li");
 
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle("open");
+  navLinks.classList.toggle("open");
 });
 
 function hover(element) {
-    element.setAttribute('src', 'img/realProfilePhoto.svg');
-  }
-  
-  function unhover(element) {
-    element.setAttribute('src', 'img/profilePhoto.svg');
-  }
+  element.setAttribute('src', 'img/realProfilePhoto.svg');
+}
 
-  var typed = new Typed('.typing',{
-    strings: ["Developer", "Fast learner", "UX/UI lover", "Team leader", "Dedicated", "Engineer"],
-    loop: true,
-    typeSpeed: 80,
-    backSpeed: 40
-  });
+function unhover(element) {
+  element.setAttribute('src', 'img/profilePhoto.svg');
+}
+
+var typed = new Typed('.typing', {
+  strings: ["Developer", "Fast learner", "UX/UI lover", "Team leader", "Dedicated", "Engineer"],
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 40
+});
